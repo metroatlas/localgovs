8afc78f928a175ebb343b5c73d36be27cdf9ab7e
Erispoe <favrebulle@gmail.com>
2014-07-03 11:31:32 +0200

    Create a mapper module

---
+++ js/mapper.js
@@ -0,0 +1,9 @@
+d3.atlas.mapper = function module() {
+	// The mapper object contains a series of d3 maps (binding values to keys, keys corresponding to geographic objects)
+
+	function exports() {
+		// Create the mapping
+	}
+
+
+}
